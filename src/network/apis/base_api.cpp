f73011fa034c4bf5dfc010d5830e0bcad953fb04
petergarud <peter.garud04@gmail.com>
2023-10-13 00:13:17 -0400

    made base api class handle default routing better

--- src/network/apis/base_api.cpp
+++ src/network/apis/base_api.cpp
@@ -3,16 +3,37 @@
 
 namespace pweb {
 
-Api::Api() { }
+Api::Api() { 
+    get_routes_.emplace("", &Api::basic);
+    post_routes_.emplace("", &Api::basic);
+}
 
 Api::Api(std::string base_route, HttpServer& server)
     : base_route_(base_route)
     , server_(&server) {
     server_->addApi(base_route_, this);
+    get_routes_.emplace("", &Api::basic);
+    post_routes_.emplace("", &Api::basic);
 }
 
 std::string Api::processRequest(std::string method, std::string path, std::string request_body) {
-    return "/Users/petergarud/Documents/Personal/Development/Projects/cppWebFramework/src/html/index.html";
+    std::string file;
+    if (method == "GET") {
+        auto val = get_routes_.find(path);
+        if (val == get_routes_.end()) {
+            return error();
+        }
+        FuncPtr func = val->second;
+        file = (this->*func)(request_body);
+    } else if (method == "POST") {
+        auto val = post_routes_.find(path);
+        if (val == get_routes_.end()) {
+            return error();
+        }
+        FuncPtr func = val->second;
+        file = (this->*func)(request_body);
+    }
+    return file;
 }
 
 } // end namespace pweb
--- src/network/apis/base_api.hpp
+++ src/network/apis/base_api.hpp
@@ -10,15 +10,23 @@ class HttpServer;
 
 class Api {
 private:
+    using FuncPtr = std::string (Api::*)(std::string);
     std::string base_route_;
     HttpServer* server_;
-    std::unordered_map<std::string, void (*)(std::string)> get_routes_;
+    std::unordered_map<std::string, FuncPtr> get_routes_;
+    std::unordered_map<std::string, FuncPtr> post_routes_;
 
 public:
     Api();
     Api(std::string base_route, HttpServer& server);
     virtual ~Api() = default;
     virtual std::string processRequest(std::string method, std::string path, std::string request_body);
+    virtual std::string basic(std::string request_body) {
+        return "/Users/petergarud/Documents/Personal/Development/Projects/cppWebFramework/src/html/index.html";
+    }
+    virtual std::string error() {
+        return "/Users/petergarud/Documents/Personal/Development/Projects/cppWebFramework/src/html/error.html";
+    }
 };
 
 } // end namespace pweb
